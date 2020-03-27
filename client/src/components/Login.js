ad55147a2d3e74f3a3dae77cd62af26d21aafc59
Alex Karavil <alex@karavil.io>
2020-03-27 11:43:04 -0400

    Added login page with form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,34 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const StyledForm = styled.form`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   width: 100%;
+   height: 40vh;
+`;
+export default function Login() {
+   const { register, handleSubmit, errors } = useForm();
+   const onSubmit = data => console.log(data);
 
-export default Login;
+   return (
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+         <h1>Login in for bubbles!</h1>
+         <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            ref={register({ required: true, maxLength: 80 })}
+         />
+         <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            ref={register({ required: true, maxLength: 80 })}
+         />
+         <input type="submit" />
+      </StyledForm>
+   );
+}
